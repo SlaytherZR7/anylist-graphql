69a76f3186780366305ef20a0ccc2c8f632581ef
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-27 11:20:23 -0500

    feat: add JWT authentication support with configuration and necessary dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
   providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' },
+      }),
+    }),
+    UsersModule,
+  ],
 })
 export class AuthModule {}
