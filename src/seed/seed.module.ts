fa406f134a24a0a7fa8ce8e8e729cd7de39a0fbd
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-18 16:59:32 -0500

    feat: add production check in SeedService to prevent seeding in production environment

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,10 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
 
 @Module({
   providers: [SeedResolver, SeedService],
+  imports: [ConfigModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
+  private isProd: boolean;
+  constructor(private readonly configService: ConfigService) {
+    this.isProd = this.configService.get('STATE') === 'prod';
+  }
   async seed(): Promise<boolean> {
+    if (this.isProd) {
+      throw new UnauthorizedException(
+        'You are not allowed to seed in production',
+      );
+    }
+
     return true;
   }
 }
