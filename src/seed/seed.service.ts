7a912de6a2fd55de078681b15d02df9e5bc12e32
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-18 17:11:36 -0500

    feat: enhance SeedService to include item and user repository injections and implement data deletion before seeding

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
-  constructor(private readonly configService: ConfigService) {
+
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
     this.isProd = this.configService.get('STATE') === 'prod';
   }
+
   async seed(): Promise<boolean> {
     if (this.isProd) {
       throw new UnauthorizedException(
@@ -14,6 +26,13 @@ export class SeedService {
       );
     }
 
+    await this.deleteData();
+
     return true;
+  }
+
+  private async deleteData() {
+    await this.itemRepository.createQueryBuilder().delete().where({}).execute();
+    await this.userRepository.createQueryBuilder().delete().where({}).execute();
   }
 }
