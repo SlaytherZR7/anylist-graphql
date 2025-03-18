5b8bf8b5f01b4519901bddb6fe929a8d65defb97
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-17 21:23:00 -0500

    feat: secure findAll query with JwtAuthGuard and CurrentUser decorator for role-based access

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,14 +1,22 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  findAll(@Args() validRoles: ValidRolesArgs): Promise<User[]> {
+  findAll(
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User,
+  ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
 
