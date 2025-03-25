46ad5323e20235ebe7c9373f67f9380b898f3f84
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-25 12:09:01 -0500

    feat: add list count and retrieval functionality for users in Users module

--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -72,4 +72,12 @@ export class ListsService {
     await this.listsRepository.remove(list);
     return { ...list, id };
   }
+
+  async listCountByUser(user: User): Promise<number> {
+    return this.listsRepository.count({
+      where: {
+        user: { id: user.id },
+      },
+    });
+  }
 }
