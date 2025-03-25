4d3a2f743f9f28f189d7e48dac34a2234603b7e1
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-25 11:53:35 -0500

    feat: enhance Lists module to include user context and support for pagination and search in list operations

--- src/lists/lists.resolver.ts
+++ src/lists/lists.resolver.ts
@@ -1,35 +1,56 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
 import { ListsService } from './lists.service';
 import { List } from './entities/list.entity';
 import { CreateListInput } from './dto/create-list.input';
 import { UpdateListInput } from './dto/update-list.input';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { SearchArgs } from 'src/common/dto/args/search.args';
+import { PaginationArgs } from 'src/common/dto/args/pagination.args';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 @Resolver(() => List)
 export class ListsResolver {
   constructor(private readonly listsService: ListsService) {}
 
   @Mutation(() => List)
-  createList(@Args('createListInput') createListInput: CreateListInput) {
-    return this.listsService.create(createListInput);
+  createList(
+    @Args('createListInput') createListInput: CreateListInput,
+    @CurrentUser() user: User,
+  ): Promise<List> {
+    return this.listsService.create(createListInput, user);
   }
 
   @Query(() => [List], { name: 'lists' })
-  findAll() {
-    return this.listsService.findAll();
+  findAll(
+    @CurrentUser() user: User,
+    @Args() paginationArgs: PaginationArgs,
+    @Args() searchArgs: SearchArgs,
+  ) {
+    return this.listsService.findAll(user, paginationArgs, searchArgs);
   }
 
   @Query(() => List, { name: 'list' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.listsService.findOne(id);
+  findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser() user: User,
+  ): Promise<List> {
+    return this.listsService.findOne(id, user);
   }
 
   @Mutation(() => List)
-  updateList(@Args('updateListInput') updateListInput: UpdateListInput) {
-    return this.listsService.update(updateListInput.id, updateListInput);
+  updateList(
+    @Args('updateListInput') updateListInput: UpdateListInput,
+    @CurrentUser() user: User,
+  ): Promise<List> {
+    return this.listsService.update(updateListInput.id, updateListInput, user);
   }
 
   @Mutation(() => List)
-  removeList(@Args('id', { type: () => Int }) id: number) {
-    return this.listsService.remove(id);
+  removeList(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser() user: User,
+  ) {
+    return this.listsService.remove(id, user);
   }
 }
--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -1,26 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateListInput } from './dto/create-list.input';
 import { UpdateListInput } from './dto/update-list.input';
+import { User } from 'src/users/entities/user.entity';
+import { PaginationArgs } from 'src/common/dto/args/pagination.args';
+import { SearchArgs } from 'src/common/dto/args/search.args';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { List } from './entities/list.entity';
 
 @Injectable()
 export class ListsService {
-  create(createListInput: CreateListInput) {
-    return 'This action adds a new list';
+  constructor(
+    @InjectRepository(List)
+    private readonly listsRepository: Repository<List>,
+  ) {}
+
+  async create(createListInput: CreateListInput, user: User) {
+    const newList = this.listsRepository.create({ ...createListInput, user });
+    return await this.listsRepository.save(newList);
   }
 
-  findAll() {
-    return `This action returns all lists`;
+  async findAll(
+    user: User,
+    paginationArgs: PaginationArgs,
+    searchArgs: SearchArgs,
+  ) {
+    const { limit, offset } = paginationArgs;
+    const { search } = searchArgs;
+
+    const queryBuilder = this.listsRepository
+      .createQueryBuilder()
+      .take(limit)
+      .skip(offset)
+      .where(`"userId" = :userId`, { userId: user.id });
+
+    if (search) {
+      queryBuilder.andWhere('LOWER(name) like :name', {
+        name: `%${search.toLowerCase()}%`,
+      });
+    }
+
+    return queryBuilder.getMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} list`;
+  async findOne(id: string, user: User) {
+    const list = await this.listsRepository.findOneBy({
+      id,
+      user: { id: user.id },
+    });
+
+    if (!list) throw new NotFoundException(`List with id: ${id} not found`);
+
+    return list;
   }
 
-  update(id: number, updateListInput: UpdateListInput) {
-    return `This action updates a #${id} list`;
+  async update(id: string, updateListInput: UpdateListInput, user: User) {
+    await this.findOne(id, user);
+
+    const list = await this.listsRepository.preload({
+      ...updateListInput,
+      user,
+    });
+
+    if (!list) throw new NotFoundException(`List with id: ${id} not found`);
+
+    return this.listsRepository.save(list);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} list`;
+  async remove(id: string, user: User) {
+    const list = await this.findOne(id, user);
+    await this.listsRepository.remove(list);
+    return { ...list, id };
   }
 }
